d589337b009b63da0b19bd58bef207c6cc8169fd
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-11-17 01:14:17 +0530

    first commit

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -19,13 +23,35 @@ const List<String> currenciesList = [
   'SEK',
   'SGD',
   'USD',
-  'ZAR'
+  'ZAR',
 ];
 
-const List<String> cryptoList = [
-  'BTC',
-  'ETH',
-  'LTC',
-];
+const List<String> cryptoList = ['BTC', 'ETH', 'LTC'];
 
-class CoinData {}
+const coinAPIURl = 'https://api.tatum.io/v4/data/rate/symbol';
+const apiKey = 't-6918c6873b9bb09e10628df8-b7f96cd9589e4cadb404dd7d';
+
+class CoinData {
+  getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      String requestUrl =
+          '$coinAPIURl?symbol=$crypto&basePair=$selectedCurrency';
+
+      var response = await http.get(
+        Uri.parse(requestUrl),
+        headers: {'x-api-key': apiKey},
+      );
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        var lastPrice = double.parse(decodedData['value'].toString());
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        print('Status Code: ${response.statusCode}');
+        print(response.body);
+        throw 'Problem with the get request';
+      }
+    }
+    return cryptoPrices;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'price_screen.dart';
 
 void main() => runApp(MyApp());
@@ -7,9 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-          primaryColor: Colors.lightBlue,
-          scaffoldBackgroundColor: Colors.white),
+        primaryColor: Colors.lightBlue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
       home: PriceScreen(),
     );
   }
